c110a0b05d0536b86e5171d5ee91b822c740a252
varun-naga <varunmulugu2000@gmail.com>
2022-03-27 22:40:18 +0530

    update main class file

--- src/main/java/com/example/course/CourseController.java
+++ src/main/java/com/example/course/CourseController.java
@@ -11,33 +11,33 @@ import java.util.Optional;
 public class CourseController {
 
     @Autowired
-    private CourseService Courseservice;
+    private CourseService courseService;
 
     @RequestMapping("/topics/{id}/courses")
     public List<Course> getAllCourses(@PathVariable String id) {
-        return Courseservice.getAllCourses(id);
+        return courseService.getAllCourses(id);
 
     }
 
     @RequestMapping("/topics/{topicId}/course/{id}")
     public Optional<Course> getCourse(@PathVariable String id) {
-        return Courseservice.getCourse(id);
+        return courseService.getCourse(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
     public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
         course.setTopic(new Topic(topicId, "", ""));
-        Courseservice.addCourse(course);
+        courseService.addCourse(course);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
     public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String id) {
         course.setTopic(new Topic(topicId, "", ""));
-        Courseservice.updateCourse(course);
+        courseService.updateCourse(course);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/courses/{id}")
     public void deleteCourse(@PathVariable String topicId, @PathVariable String id) {
-        Courseservice.deleteCourse(id);
+        courseService.deleteCourse(id);
     }
 }
